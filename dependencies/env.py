daf1add9d71c45297946fc46a53d79cbd8c4740d
Mahdi <mghorbel1@yahoo.fr>
2025-03-30 00:06:14 +0100

    added blockchain related environnement variables

--- dependencies/env.py
+++ dependencies/env.py
@@ -6,3 +6,10 @@ load_dotenv()
 
 BASE_URL = os.getenv("BASE_URL")
 DATABASE_URL = os.getenv("DATABASE_URL")
+BLOCKCHAIN_URL = os.getenv("BLOCKCHAIN_URL")
+
+# Contract details (replace with your deployed contract address)
+CONTRACT_ADDRESS = os.getenv("CONTRACT_ADDRESS")
+# Public address
+ACCOUNT = os.getenv("ACCOUNT")
+PRIVATE_KEY = os.getenv("PRIVATE_KEY")
